d6b724c47b1307b95a6d1ae5f6f676ba4f4ef4d6
archieDeveloper <arkadij.ok@gmail.com>
2015-09-25 23:27:48 +0700

    верстаю шапку

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+      title: 'Archie',
+      breadCrumbs: [
+          'Программирование',
+          'PHP',
+          'Паттерны проектирования'
+      ]
+  });
 });
 
 module.exports = router;
